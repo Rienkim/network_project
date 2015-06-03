a6e79f0c0bcd3cacf80589c0dd3211aee7282b2f
joonahn <sdeveloper@naver.com>
2015-06-03 22:17:43 +0900

    Server function added to the main function

--- main.cpp
+++ main.cpp
@@ -13,6 +13,13 @@
 #include <iostream>
 #include <string.h>
 #include <fstream>
+#include <unistd.h>
+
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
 
 using std::cout;
 using std::endl;
@@ -58,6 +65,48 @@ int main(int argc, char *argv[])
     calendar.run();
     return SUCCESS;
   }
+
+  if(strcmp("-s", argv[1])==0)
+  {
+    //Server mode 
+    int serv_sock;
+    int clnt_sock;
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+    unsigned int clnt_addr_size;
+    pthread_t thread;
+
+    if(pthread_mutex_init(&mutx, NULL))
+        error_handling("mutex error");
+
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+
+    //Server Address Setting
+    memset(&serv_addr,0x00, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(5050);
+
+    if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1)
+        error_handling("bind() error");
+
+    if(listen(serv_sock, 5)==-1)
+        error_handling("listen() error");
+
+    while(1)
+    {
+        clnt_addr_size = sizeof(clnt_addr);
+        clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+        pthread_mutex_lock(&mutx);
+        clnt_socks[clnt_number++] = clnt_sock;
+        pthread_mutex_unlock(&mutx);
+
+        pthread_create(&thread, NULL, clnt_connection,(void *)clnt_sock);
+        cout<<"New Connection : IP: "<<inet_ntoa(clnt_addr.sin_addr)<<endl;
+    }
+    return SUCCESS
+  }
+
 
   // Else load calendar.
   string filename = argv[1];
@@ -121,4 +170,58 @@ int main(int argc, char *argv[])
   calendar.run();
 
   return SUCCESS;
+}
+
+void * clnt_connection(void * arg)
+{
+    int clnt_sock = (int)arg;
+    int str_len = 0;
+    char message[BUFSIZE];
+    int i;
+    FILE * readfp;
+
+    readfp = fdopen(clnt_sock, "r");
+
+    while(!feof(readfp))
+    {
+        fgets(message, BUFSIZE-1, readfp);
+        send_message(message, strlen(message)+1);
+    }
+        //send_message(message, str_len);
+
+    pthread_mutex_lock(&mutx);
+    for(i=0;i<clnt_number;i++)
+        if(clnt_sock == clnt_socks[i])
+        {
+            for(;i<clnt_number-1;i++)
+                clnt_socks[i] = clnt_socks[i+1];
+            break;
+        }
+    clnt_number--;
+    pthread_mutex_unlock(&mutx);
+
+    close(clnt_sock);
+    return 0;
+}
+
+void send_message(char * message, int len)
+{
+    int i;
+    FILE * writefp;
+    pthread_mutex_lock(&mutx);
+    for(i=0;i<clnt_number;i++)
+    {
+        writefp = fdopen(clnt_socks[i],"w");
+        fputs(message, writefp);
+        fflush(writefp);
+        //write(clnt_socks[i], message, len);
+    }
+    pthread_mutex_unlock(&mutx);
+}
+
+void error_handling(const char* message)
+{
+    cout<<message<<endl;
+    exit(1);
+
 }
